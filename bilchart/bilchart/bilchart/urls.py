71a122c0dc825707ec350ba2694d0f2548d1944e
seungy0 <dltmdrb98@gmail.com>
2021-02-05 13:39:56 +0000

    url 수정

--- bilchart/bilchart/bilchart/urls.py
+++ bilchart/bilchart/bilchart/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chart', include('webchart.urls')),
     path('', include('webchart.urls')),
 ]
