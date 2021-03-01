763597476730a08c94c05bfb93dbc94c5ccb86cb
seungy0 <dltmdrb98@gmail.com>
2021-03-01 03:39:13 +0000

    커밋

--- bilchart/bilchart/user/models.py
+++ bilchart/bilchart/user/models.py
@@ -1,14 +1,91 @@
 from django.db import models
-
+from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager, PermissionsMixin)
 # Create your models here.
 
 
-class User(models.Model):
-    user_id = models.CharField(max_length=20)
-    user_name = models.CharField(max_length=20)
-    email = models.EmailField(max_length=30)
-    password = models.CharField(max_length=100)
-    phone = models.CharField(max_length=20)
+# class User(models.Model):
+#     user_id = models.CharField(max_length=20)
+#     user_name = models.CharField(max_length=20)
+#     email = models.EmailField(max_length=30)
+#     password = models.CharField(max_length=100)
+#     phone = models.CharField(max_length=20)
 
-    class Meta:
-        db_table = 'users'
+#     class Meta:
+#         db_table = 'users'
+
+
+# class myUser(AbstractBaseUser):
+#     user_id = models.CharField(max_length=20, unique=True)
+#     user_name = models.CharField(max_length=20)
+#     email = models.EmailField(max_length=30)
+#     password = models.CharField(max_length=100)
+#     phone = models.CharField(max_length=20)
+#     USERNAME_FIELD = 'user_id'
+#     REQUIRED_FIELDS = ['user_name', 'email', 'password', 'phone']
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def create_user(self, user_id, user_name, email, phone, password=None):
+        if not user_id:
+            raise ValueError('must have user id')
+        elif not user_name:
+            raise ValueError('must have user name')
+        elif not email:
+            raise ValueError('must have user email')
+        elif not phone:
+            raise ValueError('must have user phone')
+        user = self.model(
+            user_id=user_id,
+            user_name=user_name,
+            email=self.normalize_email(email),
+            phone=phone
+        )
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, user_id, user_name, email, phone, password):
+        user = self.create_user(
+            user_id=user_id,
+            user_name=user_name,
+            email=self.normalize_email(email),
+            phone=phone,
+            password=password
+        )
+        user.is_admin = True
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    objects = UserManager()
+    user_id = models.CharField(
+        max_length=20,
+        null=False,
+        unique=True
+    )
+    user_name = models.CharField(
+        max_length=20,
+        null=False,
+        unique=False
+    )
+    email = models.EmailField(
+        max_length=255,
+        unique=True,
+    )
+    phone = models.CharField(
+        max_length=20,
+        null=False,
+        unique=True
+    )
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    USERNAME_FIELD = 'user_name'
+    REQUIRED_FIELDS = ['user_name', 'email', 'user_id', 'phone']
--- bilchart/bilchart/user/views.py
+++ bilchart/bilchart/user/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+from django.contrib import auth
 from django.contrib.auth.hashers import make_password
 from .models import User
 # Create your views here.
@@ -23,6 +24,25 @@ def register(request):   #회원가입 페이지를 보여주기 위한 함수
             # return HttpResponse('비밀번호가 다릅니다.')
             res_data['error'] = '비밀번호가 다릅니다.'
         else:
-            user = User(user_name=username, user_id=user_id, email=email, password=make_password(password), phone=phone)
-            user.save()
+            user = User.objects.create_user(
+                user_name=username, user_id=user_id, email=email, password=make_password(password), phone=phone
+            )
         return render(request, 'user/register2.html', res_data)  # register를 요청받으면 register.html 로 응답.
+
+
+def signin(request):
+    if request.method == "GET":
+        return render(request, 'user/signin.html')
+
+    elif request.method == "POST":
+        user_id = request.POST.get('username')
+        password = request.POST.get('pass')
+        res_data = {}
+        if not (user_id and password):
+            res_data['error'] = "모든 값을 입력해야 합니다."
+        user = auth.authenticate(request, user_id=user_id, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return HttpResponseRedirect('')
+        else:
+            return render(request, 'login.html', {'error': 'username or password is incorrect'})
